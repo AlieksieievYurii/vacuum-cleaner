e1f1e46de29ef3a12551e824585b4777858eb34b
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-09-12 18:01:40 +0200

    Create a list

--- android-app/app/src/main/java/com/yurii/vaccumcleaner/MainActivity.kt
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/MainActivity.kt
@@ -2,10 +2,21 @@ package com.yurii.vaccumcleaner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.yurii.vaccumcleaner.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val adapter by lazy { Adapter() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val list = listOf(BluetoothDeviceItem("test"), BluetoothDeviceItem("test2"))
+        binding.recyclerView.adapter = adapter
+        binding.search.setOnClickListener {
+
+        }
+
+        adapter.submitList(list)
     }
 }
