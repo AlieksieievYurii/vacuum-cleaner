30f60ce28e49d52016bf2cd49abbfd85ccb008d1
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-09-18 17:01:49 +0200

    Init Android project project

--- android-app/app/src/main/java/com/yurii/vaccumcleaner/MainActivity.kt
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.yurii.vaccumcleaner
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothAdapter.ACTION_DISCOVERY_STARTED
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.yurii.vaccumcleaner.databinding.ActivityMainBinding
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -18,5 +26,50 @@ class MainActivity : AppCompatActivity() {
         }
 
         adapter.submitList(list)
+
+        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+        if (bluetoothAdapter?.isEnabled == false) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, 1)
+        }
+
+        val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
+        pairedDevices?.forEach { device ->
+            val deviceName = device.name
+            val deviceHardwareAddress = device.address // MAC address
+            Timber.i("$deviceName -> $deviceHardwareAddress")
+        }
+
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        registerReceiver(receiver, filter)
+        registerReceiver(receiver, IntentFilter(ACTION_DISCOVERY_STARTED))
+
+        Timber.i("Is Discovering: ${bluetoothAdapter?.startDiscovery()}")
+    }
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private val receiver = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            val action: String = intent.action!!
+            Timber.i("Action: $action")
+            when (action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    // Discovery has found a device. Get the BluetoothDevice
+                    // object and its info from the Intent.
+                    val device: BluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)!!
+                    val deviceName = device.name
+                    val deviceHardwareAddress = device.address // MAC address
+                    Timber.i("Found: $deviceName -> $deviceHardwareAddress")
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(receiver)
     }
 }
