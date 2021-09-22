c2d863434e1ae952b03a499974a0472ab5f2837f
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-09-22 20:24:41 +0200

    Add a few extensions: flowLifecycler observer, update list for StateFlow

---
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/Extensions.kt
@@ -0,0 +1,46 @@
+package com.yurii.vaccumcleaner
+
+import androidx.lifecycle.*
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+class FlowObserver<T>(
+    private val lifecycleOwner: LifecycleOwner,
+    private val flow: Flow<T>,
+    private val collector: suspend (T) -> Unit
+) : LifecycleObserver {
+    private var job: Job? = null
+
+    init {
+        lifecycleOwner.lifecycle.addObserver(this)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    private fun onStart() {
+        job = lifecycleOwner.lifecycleScope.launch {
+            flow.collectLatest { collector(it) }
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    private fun onStop() {
+        job?.cancel()
+        job = null
+    }
+}
+
+inline fun <reified T> Flow<T>.observeOnLifecycle(
+    lifecycleOwner: LifecycleOwner,
+    noinline collector: suspend (T) -> Unit
+) = FlowObserver(lifecycleOwner, this, collector)
+
+fun <T> MutableStateFlow<List<T>>.update(newList: List<T>) {
+    this.value = this.value + newList
+}
+
+fun <T> MutableStateFlow<List<T>>.update(item: T) {
+    this.value = this.value + item
+}
