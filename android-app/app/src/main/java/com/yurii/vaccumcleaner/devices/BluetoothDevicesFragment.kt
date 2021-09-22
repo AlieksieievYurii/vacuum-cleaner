49cf4a341d07cff0ce14e7089b0015dd106a129b
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-09-22 20:25:47 +0200

    Create viewModel and move all logic stuff there

--- android-app/app/src/main/java/com/yurii/vaccumcleaner/devices/BluetoothDevicesFragment.kt
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/devices/BluetoothDevicesFragment.kt
@@ -1,19 +1,39 @@
 package com.yurii.vaccumcleaner.devices
 
+import android.bluetooth.BluetoothDevice
+import android.content.IntentFilter
 import android.os.Bundle
 import android.view.View
 import android.viewbinding.library.fragment.viewBinding
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.yurii.vaccumcleaner.R
 import com.yurii.vaccumcleaner.databinding.BluetoothDevicesFragmentBinding
+import com.yurii.vaccumcleaner.observeOnLifecycle
 
 class BluetoothDevicesFragment : Fragment(R.layout.bluetooth_devices_fragment) {
+    private val viewModel: BluetoothDevicesViewModel by viewModels { BluetoothDevicesViewModel.Factory() }
     private val binding: BluetoothDevicesFragmentBinding by viewBinding()
     private val adapter = Adapter()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.bluetoothDevices.adapter = adapter
-        adapter.submitList(listOf(BluetoothDeviceItem("Test", "2344", isPaired = true)))
+        requireActivity().registerReceiver(viewModel.broadcastReceiverBluetoothDeviceFound, IntentFilter(BluetoothDevice.ACTION_FOUND))
+
+        viewModel.bluetoothDevices.observeOnLifecycle(viewLifecycleOwner) { adapter.submitList(it) }
+        viewModel.bluetoothState.observeOnLifecycle(viewLifecycleOwner) {
+            when (it) {
+                BluetoothDevicesViewModel.BluetoothState.BluetoothIsDisabled -> {}
+                BluetoothDevicesViewModel.BluetoothState.BluetoothIsUnsupported -> TODO()
+                BluetoothDevicesViewModel.BluetoothState.None -> {
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        requireActivity().unregisterReceiver(viewModel.broadcastReceiverBluetoothDeviceFound)
     }
 }
---
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/devices/BluetoothDevicesViewModel.kt
@@ -0,0 +1,69 @@
+package com.yurii.vaccumcleaner.devices
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.yurii.vaccumcleaner.update
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class BluetoothDevicesViewModel : ViewModel() {
+    sealed class BluetoothState {
+        object None : BluetoothState()
+        object BluetoothIsDisabled : BluetoothState()
+        object BluetoothIsUnsupported : BluetoothState()
+    }
+
+    private val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+
+    val broadcastReceiverBluetoothDeviceFound = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val device: BluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)!!
+            _bluetoothDevices.update(BluetoothDeviceItem(name = device.name, macAddress = device.address, isPaired = false))
+        }
+    }
+
+    private val _bluetoothDevices: MutableStateFlow<List<BluetoothDeviceItem>> = MutableStateFlow(emptyList())
+    val bluetoothDevices: StateFlow<List<BluetoothDeviceItem>> = _bluetoothDevices
+
+    private val _bluetoothState: MutableStateFlow<BluetoothState> = MutableStateFlow(BluetoothState.None)
+    val bluetoothState: StateFlow<BluetoothState> = _bluetoothState
+
+//    private val eventChannel = Channel<Event>(Channel.BUFFERED)
+//    val eventFlow: Flow<Event> = eventChannel.receiveAsFlow()
+
+    init {
+        if (bluetoothAdapter == null)
+            _bluetoothState.value = BluetoothState.BluetoothIsUnsupported
+        else {
+            if (bluetoothAdapter.isEnabled) {
+                bluetoothAdapter.startDiscovery()
+                loadPairedDevices()
+            } else
+                _bluetoothState.value = BluetoothState.BluetoothIsDisabled
+        }
+    }
+
+    private fun loadPairedDevices() {
+        val devices = bluetoothAdapter!!.bondedDevices.map {
+            BluetoothDeviceItem(name = it.name, macAddress = it.address, isPaired = true)
+        }
+
+        _bluetoothDevices.update(devices)
+    }
+
+
+    class Factory() : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(BluetoothDevicesViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return BluetoothDevicesViewModel() as T
+            }
+            throw IllegalArgumentException("Unable to construct viewModel")
+        }
+    }
+}
