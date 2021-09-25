8b477450de117efd3f5437601cbdd95102e660e6
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-09-25 14:10:52 +0200

    Add necessary args to Fragments

--- android-app/app/src/main/java/com/yurii/vaccumcleaner/screens/settings/SettingsFragment.kt
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/screens/settings/SettingsFragment.kt
@@ -5,15 +5,19 @@ import android.view.View
 import android.viewbinding.library.fragment.viewBinding
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.yurii.vaccumcleaner.R
 import com.yurii.vaccumcleaner.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
     private val binding: FragmentSettingsBinding by viewBinding()
+    private val args: SettingsFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.setWifi.setOnClickListener { findNavController().navigate(R.id.action_settingsFragment_to_wifiSettingsFragment) }
+        binding.setWifi.setOnClickListener {
+            findNavController().navigate(SettingsFragmentDirections.actionSettingsFragmentToWifiSettingsFragment(args.bluetoothDevice))
+        }
     }
 }
--- android-app/app/src/main/java/com/yurii/vaccumcleaner/screens/wifi/WifiSettingsFragment.kt
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/screens/wifi/WifiSettingsFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.View
 import android.viewbinding.library.fragment.viewBinding
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.yurii.vaccumcleaner.R
 import com.yurii.vaccumcleaner.databinding.FragmentWifiSettingsBinding
 
 class WifiSettingsFragment : Fragment(R.layout.fragment_wifi_settings) {
     private val binding: FragmentWifiSettingsBinding by viewBinding()
+    private val args: WifiSettingsFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
