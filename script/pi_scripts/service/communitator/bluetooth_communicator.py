92cf42d88031ca66a7a06cf31957c74eea43b74f
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-10-03 21:12:50 +0200

    Add logging to bluetooth_communicator

--- script/pi_scripts/service/communitator/bluetooth_communicator.py
+++ script/pi_scripts/service/communitator/bluetooth_communicator.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from threading import Thread
 from typing import Dict
 
@@ -15,21 +16,22 @@ class BluetoothCommunicator(Communicator):
         self.server_sock.listen(1)
         self.client_sock = None
 
-    def connect(self):
+    def _connect(self):
+        logging.info('Start connecting...')
         bluetooth.advertise_service(self.server_sock, "SampleServer", service_id=self.UUID,
                                     service_classes=[self.UUID, bluetooth.SERIAL_PORT_CLASS],
                                     profiles=[bluetooth.SERIAL_PORT_PROFILE])
-        print("Waiting for connection on RFCOMM channel", self.server_sock.getsockname()[1])
+        logging.info("Waiting for connection on RFCOMM channel", self.server_sock.getsockname()[1])
         client_sock, client_info = self.server_sock.accept()
-        print("Accepted connection from", client_info)
+        logging.info("Accepted connection from", client_info)
         self.client_sock = client_sock
 
     def send(self, data: Dict) -> None:
-        print(f'Data to be send: {data}')
+        logging.debug(f'Send data: {data}')
         self.client_sock.send(json.dumps(data))
 
     def listen_output(self, call_back) -> None:
-        self.connect()
+        self._connect()
 
         def inner_callback():
             while True:
@@ -39,3 +41,4 @@ class BluetoothCommunicator(Communicator):
                 call_back(data.decode().rstrip())
 
         Thread(target=inner_callback, daemon=True).start()
+        logging.debug('Started listening for the output.')
