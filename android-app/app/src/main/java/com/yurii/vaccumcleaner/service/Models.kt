59aa3ce6961fadd0949a922df18b96ecea3e8931
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-10-09 15:05:46 +0200

    Implement RequestHandler

--- android-app/app/src/main/java/com/yurii/vaccumcleaner/service/Models.kt
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/service/Models.kt
@@ -2,7 +2,8 @@ package com.yurii.vaccumcleaner.service
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import kotlin.reflect.KClass
+import com.squareup.moshi.Moshi
+import org.json.JSONObject
 
 @JsonClass(generateAdapter = false)
 enum class ResponseStatus {
@@ -47,6 +48,28 @@ data class Response<R : Any>(
     val response: R?
 )
 
-abstract class RequestHandler<R : Any, P : Any>(private val requestName: String, responseModelClass: KClass<R>, parameters: KClass<P>?) {
-    abstract fun handle(request: Request<P>): R
+abstract class RequestHandler<R : Any, P : Any>(
+    val requestName: String,
+    private val responseModelClass: Class<R>,
+    private val parameters: Class<P>?
+) {
+    fun handleIncomingRequest(moshi: Moshi, request: Request<*>): String {
+        val inputParameters: P? =
+            if (parameters != null) moshi.adapter(parameters).fromJson(JSONObject(request.parameters as Map<*, *>).toString())!! else null
+        val output = handle(request, inputParameters)
+        val response = Response(
+            requestId = request.requestId,
+            requestName = requestName,
+            status = ResponseStatus.OK,
+            errorMessage = null,
+            response = output
+        )
+        val packet = Packet(
+            type = PacketType.RESPONSE,
+            content = response
+        )
+        return moshi.createResponseModelAdapter(responseModelClass).toJson(packet)
+    }
+
+    abstract fun handle(request: Request<*>, parameters: P?): R
 }
--- android-app/app/src/main/java/com/yurii/vaccumcleaner/service/Service.kt
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/service/Service.kt
@@ -6,7 +6,8 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
-import timber.log.Timber
+import java.lang.Exception
+import java.lang.IllegalStateException
 import java.util.concurrent.TimeoutException
 
 fun <R : Any> Moshi.createResponseModelAdapter(responseClass: Class<R>): JsonAdapter<Packet<Response<R>>> =
@@ -15,18 +16,32 @@ fun <R : Any> Moshi.createResponseModelAdapter(responseClass: Class<R>): JsonAda
 fun <R : Any> Moshi.createParametrizedRequestAdapter(parameters: Class<R>): JsonAdapter<Request<R>> =
     this.adapter(Types.newParameterizedType(Request::class.java, parameters))
 
-class Service(private val coroutineScope: CoroutineScope, bluetoothDevice: BluetoothDevice, private val requestHandlers: List<RequestHandler<*, *>>) {
+class Service(
+    private val coroutineScope: CoroutineScope,
+    bluetoothDevice: BluetoothDevice,
+    private val requestHandlers: List<RequestHandler<*, *>> = emptyList()
+) {
     private val communicator = BluetoothCommunicator(bluetoothDevice)
 
     private val moshi = Moshi.Builder().build()
 
-    private val packetAdapter = moshi.adapter<Packet<Any>>(Types.newParameterizedType(Packet::class.java, Any::class.java))
-    private val packetModelAdapter = moshi.adapter<Packet<Response<Any>>>(
+    private val packetAdapter = moshi.adapter<Packet<*>>(Types.newParameterizedType(Packet::class.java, Any::class.java))
+    private val packetResponseAdapter = moshi.adapter<Packet<Response<Any>>>(
         Types.newParameterizedType(Packet::class.java, Types.newParameterizedType(Response::class.java, Any::class.java))
     )
-    private val noParametrizedRequestAdapter = moshi.adapter<Packet<Request<Any>>>(
+
+    private val packetRequestAdapter = moshi.adapter<Packet<Request<*>>>(
         Types.newParameterizedType(Packet::class.java, Types.newParameterizedType(Request::class.java, Any::class.java))
     )
+    private val noParametrizedRequestAdapter = moshi.adapter<Packet<Request<*>>>(
+        Types.newParameterizedType(Packet::class.java, Types.newParameterizedType(Request::class.java, Any::class.java))
+    )
+
+    private val _receivedPackets: MutableSharedFlow<Packet<*>> = MutableSharedFlow()
+    val receivedPackets = _receivedPackets.asSharedFlow()
+
+    private val _brokenPacket: MutableSharedFlow<Pair<String, Exception>> = MutableSharedFlow()
+    val brokenPackets = _brokenPacket.asSharedFlow()
 
     suspend fun start() {
         communicator.connect()
@@ -34,17 +49,41 @@ class Service(private val coroutineScope: CoroutineScope, bluetoothDevice: Bluet
             communicator.startListening()
         }
         coroutineScope.launch {
-            communicator.output.collect {
-                Timber.d("Data received: $it")
+            communicator.output.collect { packetJson ->
+                try {
+                    val packet = parsePacket(packetJson)
+                    _receivedPackets.emit(packet)
+                    handleIfRequest(packet)
+                } catch (error: Exception) {
+                    _brokenPacket.emit(packetJson to error)
+                }
             }
         }
     }
 
-//    private fun parseAsRequest(json: String): Request<*> {
-//        val packet = packetAdapter.fromJson(jsonResponse)!!
-////        if (packet.type == PacketType.REQUEST)
-////            return null
-//    }
+    private fun handleIfRequest(packet: Packet<*>) {
+        if (packet.type == PacketType.RESPONSE)
+            return
+        val request = packet.content as Request<*>
+        val requestHandler = requestHandlers.find { it.requestName == request.requestName }
+            ?: throw IllegalStateException("Cannot find request handler for ${request.requestName}")
+        handleRequest(request, requestHandler)
+    }
+
+    private fun handleRequest(request: Request<*>, requestHandler: RequestHandler<*, *>) {
+        coroutineScope.launch(Dispatchers.IO) {
+            val response = requestHandler.handleIncomingRequest(moshi, request)
+            communicator.send(response)
+        }
+    }
+
+    private fun parsePacket(json: String): Packet<*> {
+        val packet = packetAdapter.fromJson(json)!!
+        return when (packet.type) {
+            PacketType.REQUEST -> packetRequestAdapter.fromJson(json)!!
+            PacketType.RESPONSE -> packetResponseAdapter.fromJson(json)!!
+        }
+    }
 
     suspend fun <R : Any, P : Any> request(
         requestName: String,
@@ -65,7 +104,7 @@ class Service(private val coroutineScope: CoroutineScope, bluetoothDevice: Bluet
     }
 
     suspend fun <R : Any> request(requestName: String, responseClass: Class<R>, timeout: Long = 10000L): R = withContext(Dispatchers.IO) {
-        val request = Request<Any>(
+        val request = Request(
             requestName = requestName,
             requestId = System.currentTimeMillis(),
             parameters = null
@@ -86,9 +125,9 @@ class Service(private val coroutineScope: CoroutineScope, bluetoothDevice: Bluet
         if (packet.type == PacketType.REQUEST)
             return null
 
-        val r = packetModelAdapter.fromJson(jsonResponse)!!.content
+        val responsePacket = packetResponseAdapter.fromJson(jsonResponse)!!.content
 
-        if (r.requestId != request.requestId || r.requestName != request.requestName)
+        if (responsePacket.requestId != request.requestId || responsePacket.requestName != request.requestName)
             return null
 
         val packetRespAdapter = moshi.createResponseModelAdapter(responseClass)
