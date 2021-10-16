e7789a3c3d71974353f13b8ad1c6356b001c0d3f
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-10-16 18:24:41 +0200

    Create a main activity in order to share data between fragments

--- android-app/app/src/main/java/com/yurii/vaccumcleaner/MainActivity.kt
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/MainActivity.kt
@@ -2,15 +2,16 @@ package com.yurii.vaccumcleaner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.viewbinding.library.activity.viewBinding
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.databinding.DataBindingUtil
 import com.yurii.vaccumcleaner.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    private val viewModel: MainViewModel by viewModels { MainViewModel.Factory() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        viewModel
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
