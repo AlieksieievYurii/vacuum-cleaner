dfde85d7a4b418c3fe5b8d0d47470f03842cd16b
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2021-10-16 16:01:32 +0200

    Prove of concept: Wifi settings

--- android-app/app/src/main/java/com/yurii/vaccumcleaner/screens/wifi/WifiSettingsFragment.kt
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/screens/wifi/WifiSettingsFragment.kt
@@ -3,11 +3,14 @@ package com.yurii.vaccumcleaner.screens.wifi
 import android.os.Bundle
 import android.view.View
 import android.viewbinding.library.fragment.viewBinding
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.yurii.vaccumcleaner.R
 import com.yurii.vaccumcleaner.databinding.FragmentWifiSettingsBinding
+import com.yurii.vaccumcleaner.observeOnLifecycle
 
 
 class WifiSettingsFragment : Fragment(R.layout.fragment_wifi_settings) {
@@ -16,10 +19,64 @@ class WifiSettingsFragment : Fragment(R.layout.fragment_wifi_settings) {
     private val viewModel: WifiSettingsViewModel by viewModels { WifiSettingsViewModel.Factory(args.bluetoothDevice) }
     private val binding: FragmentWifiSettingsBinding by viewBinding()
 
-
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        viewModel.state.observeOnLifecycle(viewLifecycleOwner) { state ->
+            when (state) {
+                is WifiSettingsViewModel.State.Error -> binding.apply {
+                    loading.isVisible = false
+                    image.isVisible = true
+                    image.setImageResource(R.drawable.ic_error)
+                    hint.isVisible = true
+                    hint.text = getString(R.string.label_error_message, state.message)
+                    inputState(isEnabled = true)
+                }
+                WifiSettingsViewModel.State.LoadingWifiSettings -> binding.apply {
+                    loading.isVisible = true
+                    image.isVisible = false
+                    hint.text = getString(R.string.hint_loading_cuurent_configuration)
+                    inputState(isEnabled = false)
+                }
+                WifiSettingsViewModel.State.None -> binding.apply {
+                    hint.isVisible = false
+                    loading.isVisible = false
+                    image.isVisible = true
+                    image.setImageResource(R.drawable.ic_wifi)
+                    inputState(isEnabled = true)
+                }
+                WifiSettingsViewModel.State.ApplyingWifiSettings -> binding.apply {
+                    loading.isVisible = true
+                    image.isVisible = false
+                    hint.isVisible = true
+                    hint.text = getString(R.string.hint_applying_wifi_settings)
+                    inputState(isEnabled = false)
+                }
+            }
+        }
+
+        viewModel.eventFlow.observeOnLifecycle(viewLifecycleOwner) { event ->
+            when (event) {
+                WifiSettingsViewModel.Event.SettingsHaveBeenApplied -> Toast.makeText(
+                    requireContext(),
+                    "Wifi setup has been applied!",
+                    Toast.LENGTH_SHORT
+                ).show()
+                WifiSettingsViewModel.Event.CurrentSetupHasBeenReturned -> Toast.makeText(
+                    requireContext(),
+                    "Current Wifi setup has been returned",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun inputState(isEnabled: Boolean) {
+        binding.apply {
+            ssid.isEnabled = isEnabled
+            password.isEnabled = isEnabled
+            apply.isEnabled = isEnabled
+        }
     }
 }
--- android-app/app/src/main/java/com/yurii/vaccumcleaner/screens/wifi/WifiSettingsViewModel.kt
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/screens/wifi/WifiSettingsViewModel.kt
@@ -1,23 +1,79 @@
 package com.yurii.vaccumcleaner.screens.wifi
 
 import android.bluetooth.BluetoothDevice
+import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.yurii.vaccumcleaner.Empty
+import com.yurii.vaccumcleaner.SetWifiResponse
+import com.yurii.vaccumcleaner.WifiSettings
 import com.yurii.vaccumcleaner.service.Service
+import com.yurii.vaccumcleaner.value
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
-
+import java.lang.Exception
 
 
 class WifiSettingsViewModel(bluetoothDevice: BluetoothDevice) : ViewModel() {
+    sealed class State {
+        object None : State()
+        object LoadingWifiSettings : State()
+        object ApplyingWifiSettings : State()
+        data class Error(val message: String) : State()
+    }
+
+    sealed class Event {
+        object CurrentSetupHasBeenReturned : Event()
+        object SettingsHaveBeenApplied : Event()
+    }
+
+    val ssidField = ObservableField(String.Empty)
+    val passwordField = ObservableField(String.Empty)
+
+    private val _state: MutableStateFlow<State> = MutableStateFlow(State.LoadingWifiSettings)
+    val state = _state.asStateFlow()
+
+    private val eventChannel = Channel<Event>(Channel.BUFFERED)
+    val eventFlow = eventChannel.receiveAsFlow()
+
     private val service = Service(viewModelScope, bluetoothDevice, listOf())
 
     init {
         viewModelScope.launch {
             service.start()
+            try {
+                val wifiSettings = service.request("get_wifi_settings", WifiSettings::class.java)
+                _state.value = State.None
+                ssidField.set(wifiSettings.ssid)
+                passwordField.set(wifiSettings.password)
+            } catch (error: Exception) {
+                _state.value = State.Error(error.message ?: "No error message!")
+            }
         }
     }
 
+    fun applyWifiSettings() {
+        viewModelScope.launch {
+            _state.value = State.ApplyingWifiSettings
+            val wifiSettings = WifiSettings(ssid = ssidField.value, password = passwordField.value)
+            try {
+                val resp = service.request(
+                    "set_wifi_settings", wifiSettings, SetWifiResponse::class.java, WifiSettings::class.java,
+                    timeout = 13000
+                )
+                if (resp.isConnected) {
+                    _state.value = State.None
+                    eventChannel.send(Event.SettingsHaveBeenApplied)
+                }
+            } catch (error: Exception) {
+                _state.value = State.Error(error.message ?: "No error message!")
+            }
+        }
+    }
 
     class Factory(private val bluetoothDevice: BluetoothDevice) : ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
