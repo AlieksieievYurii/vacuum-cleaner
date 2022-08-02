5aeed5b426aa22efe4223f4a5723bc11e62a110e
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2022-08-02 10:29:57 +0200

    Add parsing rangefinder values from A1

--- motherboard/core/a1/models.py
+++ motherboard/core/a1/models.py
@@ -105,6 +105,9 @@ class A1Data(object):
         self.end_left_trig: bool = False
         self.end_dust_box_trig: bool = False
         self.end_lid_trig: bool = False
+        self.rangefinder_left_value: int = 0
+        self.rangefinder_center_value: int = 0
+        self.rangefinder_right_value: int = 0
 
     def parse_and_refresh(self, string: str):
         for result in self.__PATTERN.findall(string):
@@ -113,6 +116,8 @@ class A1Data(object):
                 self._parse_and_set_buttons_state(int(result[1]))
             elif sensor_id == '2':
                 self._parse_and_set_ends_state(int(result[1]))
+            elif sensor_id == '3':
+                self._parse_dis_values(int(result[1]))
 
     def _parse_and_set_buttons_state(self, value: int) -> None:
         # TODO implement
@@ -125,3 +130,8 @@ class A1Data(object):
         self.end_left_trig = bool(value & 0x2)
         self.end_dust_box_trig = bool(value & 0x4)
         self.end_lid_trig = bool(value & 0x8)
+
+    def _parse_dis_values(self, value: int) -> None:
+        self.rangefinder_left_value = value & 0xFF
+        self.rangefinder_center_value = value >> 0x8 & 0xFF
+        self.rangefinder_right_value = value >> 0x10 & 0xFF
