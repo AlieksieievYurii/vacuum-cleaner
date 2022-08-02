1cf587e914ae4a8148f3de9689a633163e5c7351
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2022-08-02 14:18:06 +0200

    Create empty initial fragment

---
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/screens/loading/InitialFragment.kt
@@ -0,0 +1,7 @@
+package com.yurii.vaccumcleaner.screens.loading
+
+import androidx.fragment.app.Fragment
+import com.yurii.vaccumcleaner.R
+
+class InitialFragment : Fragment(R.layout.fragment_initial) {
+}
