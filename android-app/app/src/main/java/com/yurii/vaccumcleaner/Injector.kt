0b1fd0645c331574f471d2c57690013b843bc27c
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2022-08-07 16:30:25 +0200

    Add viewmodel providers

--- android-app/app/src/main/java/com/yurii/vaccumcleaner/Injector.kt
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/Injector.kt
@@ -3,8 +3,10 @@ package com.yurii.vaccumcleaner
 import android.content.Context
 import com.yurii.vaccumcleaner.robot.RobotSocketDiscovery
 import com.yurii.vaccumcleaner.screens.binder.BinderViewModel
+import com.yurii.vaccumcleaner.screens.control.ManualControlViewModel
 
 import com.yurii.vaccumcleaner.screens.loading.InitialFragmentViewModel
+import com.yurii.vaccumcleaner.screens.panel.PanelViewModel
 
 object Injector {
 
@@ -15,5 +17,13 @@ object Injector {
     fun provideInitialFragmentViewModel(context: Context): InitialFragmentViewModel.Factory {
         val robotSocketDiscovery = RobotSocketDiscovery(context)
         return InitialFragmentViewModel.Factory(context, robotSocketDiscovery)
+    }
+
+    fun providePanelViewModel(): PanelViewModel.Factory {
+        return PanelViewModel.Factory()
+    }
+
+    fun provideManualControlViewModel(): ManualControlViewModel.Factory {
+        return ManualControlViewModel.Factory()
     }
 }
