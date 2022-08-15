22764428257ad98a3c7dd1d7fa6fa3aae3895441
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2022-08-15 17:02:15 +0200

    Implement listeners for buttons of robot movements

--- android-app/app/src/main/java/com/yurii/vaccumcleaner/screens/control/ManualControlFragment.kt
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/screens/control/ManualControlFragment.kt
@@ -1,13 +1,31 @@
 package com.yurii.vaccumcleaner.screens.control
 
+import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
 import android.viewbinding.library.fragment.viewBinding
+import android.widget.SeekBar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.yurii.vaccumcleaner.Injector
 import com.yurii.vaccumcleaner.R
 import com.yurii.vaccumcleaner.databinding.FragmentControlBinding
+import com.yurii.vaccumcleaner.utils.setPressedUnpressedListener
 
 class ManualControlFragment : Fragment(R.layout.fragment_control) {
     private val binding: FragmentControlBinding by viewBinding()
-    private val viewModel: ManualControlViewModel by viewModels { Injector.provideManualControlViewModel() }
+    private val viewModel: ManualControlViewModel by viewModels { Injector.provideManualControlViewModel(requireActivity()) }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel
+
+        binding.btnForward.setPressedUnpressedListener(onPress = viewModel::moveForward, onRelease = viewModel::stop)
+        binding.btnBackward.setPressedUnpressedListener(onPress = viewModel::moveBackward, onRelease = viewModel::stop)
+        binding.btnLeft.setPressedUnpressedListener(onPress = viewModel::turnLeft, onRelease = viewModel::stop)
+        binding.btnRight.setPressedUnpressedListener(onPress = viewModel::turnRight, onRelease = viewModel::stop)
+
+
+    }
 }
--- android-app/app/src/main/java/com/yurii/vaccumcleaner/screens/control/ManualControlViewModel.kt
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/screens/control/ManualControlViewModel.kt
@@ -2,14 +2,48 @@ package com.yurii.vaccumcleaner.screens.control
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.yurii.vaccumcleaner.requesthandler.Communicator
+import com.yurii.vaccumcleaner.requesthandler.RequestHandler
+import com.yurii.vaccumcleaner.robot.Robot
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
-class ManualControlViewModel : ViewModel() {
+class ManualControlViewModel(communicator: Communicator) : ViewModel() {
+    private val robot = Robot(RequestHandler(communicator, viewModelScope).also { it.start() })
+
+    init {
+        viewModelScope.launch {
+            val r = robot.getSysInfo()
+            Timber.d(r.toString())
+        }
+    }
+
+    fun moveForward() {
+
+    }
+
+    fun moveBackward() {
+
+    }
+
+    fun turnLeft() {
+
+    }
+
+    fun turnRight() {
+
+    }
+
+    fun stop() {
+
+    }
 
     @Suppress("UNCHECKED_CAST")
-    class Factory : ViewModelProvider.Factory {
+    class Factory(private val communicator: Communicator) : ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
             if (modelClass.isAssignableFrom(ManualControlViewModel::class.java))
-                return ManualControlViewModel() as T
+                return ManualControlViewModel(communicator) as T
             throw IllegalStateException("Given the model class is not assignable from ManualControlViewModel class")
         }
     }
