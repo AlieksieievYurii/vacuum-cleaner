58d33d2ddf63550af46085fab0dd9b43d3084963
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2022-08-20 11:09:48 +0200

    Register motor wifi endpoint

--- motherboard/core/main.py
+++ motherboard/core/main.py
@@ -1,13 +1,24 @@
+from a1.robot import Robot
+from a1.socket import A1Socket
 from utils.request_handler.handler import RequestHandlerService
 from wifi.comunicator import WifiCommunicator
 from wifi.endpoints.hello_world import HelloWorldRequest
 from utils.logger import wifi_module_logger
+from wifi.endpoints.motor import Motor
+from wifi.endpoints.sys_info import GetRobotSysInfo
 
 
 def main():
+    a1_socket = A1Socket()
+    a1_socket.open()
+
+    robot = Robot(a1_socket)
+
     wifi_communicator = WifiCommunicator()
     wifi_request_handler_service = RequestHandlerService(wifi_communicator, wifi_module_logger)
     wifi_request_handler_service.register(HelloWorldRequest())
+    wifi_request_handler_service.register(GetRobotSysInfo())
+    wifi_request_handler_service.register(Motor(robot))
 
     wifi_communicator.accept_connection()
     wifi_request_handler_service.start()
