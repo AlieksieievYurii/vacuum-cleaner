9d5385834df40166b0b790124607d0134f85b530
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2022-08-20 23:25:27 +0200

    Create boolean type parsion: conver string such as 'true', 'false' to boolean

--- motherboard/core/utils/utils.py
+++ motherboard/core/utils/utils.py
@@ -1,4 +1,19 @@
 import time
 
+
 def millis() -> int:
     return round(time.time() * 1000)
+
+
+def boolean(value) -> bool:
+    if isinstance(value, bool):
+        return value
+    elif isinstance(value, str):
+        if value.lower() == 'false':
+            return False
+        elif value.lower() == 'true':
+            return True
+        else:
+            raise ValueError(f'Cannot convert string value "{value}" to Boolean')
+    else:
+        raise ValueError(f'Cannot "{value}" to Boolean')
