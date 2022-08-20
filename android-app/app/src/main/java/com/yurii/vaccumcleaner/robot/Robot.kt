129e3bc8b1946b29e0ce65d5a0949a60862bb160
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2022-08-20 12:09:08 +0200

    AndroidApp: add funs to set motors

--- android-app/app/src/main/java/com/yurii/vaccumcleaner/robot/Robot.kt
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/robot/Robot.kt
@@ -1,11 +1,29 @@
 package com.yurii.vaccumcleaner.robot
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 import com.yurii.vaccumcleaner.requesthandler.RequestHandler
+
+@JsonClass(generateAdapter = true)
+data class MotorRequestModule(@Json(name = "motor_name") val motorName: String, val value: Int)
 
 class Robot(private val requestHandler: RequestHandler) {
 
     suspend fun getSysInfo(): GeneralSystemInfo {
-        return requestHandler.send("/get-sys-info", null, GeneralSystemInfo::class.java)
+        return requestHandler.send("/get-sys-info", null, GeneralSystemInfo::class.java)!!
     }
 
+    suspend fun walkForward(speedCmPerMinute: Int) {
+        //TODO
+    }
+
+    suspend fun walkBackward(speedCmPerMinute: Int) {
+        //TODO
+    }
+
+    suspend fun setMainBrushMotor(value: Int) = setMotor("main_brush", value)
+
+    private suspend fun setMotor(motorName: String, value: Int) {
+        requestHandler.send<Any>("/set-motor", MotorRequestModule(motorName, value), null)
+    }
 }
