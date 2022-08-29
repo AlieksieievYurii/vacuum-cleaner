7231a6c276f9ccd46e65b3c5e7d8511f6a83eb0c
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2022-08-29 17:57:49 +0200

    Core: Add robot to core class

--- motherboard/core/main.py
+++ motherboard/core/main.py
@@ -16,12 +16,21 @@ WIFI_SOCKET_PORT = 1489
 
 
 class Core(object):
-    def __init__(self, wifi_endpoints_handler: WifiEndpointsHandler):
-        self.wifi_endpoints_handler = wifi_endpoints_handler
-        self.wifi_endpoints_handler.register_endpoint(HelloWorldRequest())
+    def __init__(self, robot: Robot, wifi_endpoints_handler: WifiEndpointsHandler):
+        self._robot = robot
+        self._wifi_endpoints_handler = wifi_endpoints_handler
+        self._wifi_endpoints_handler.register_endpoint(HelloWorldRequest())
+        # wifi_request_handler_service = RequestHandlerService(wifi_communicator, wifi_module_logger)
+        # wifi_request_handler_service.register(HelloWorldRequest())
+        # wifi_request_handler_service.register(GetRobotSysInfo())
+        # wifi_request_handler_service.register(Motor(robot))
+        # wifi_request_handler_service.register(Movement(robot))
+        # wifi_request_handler_service.register(StopMovement(robot))
+        # wifi_request_handler_service.register(GetA1DataRequestHandler(robot))
 
     def run(self) -> None:
-        self.wifi_endpoints_handler.start()
+        self._robot.connect()
+        self._wifi_endpoints_handler.start()
         self._run_core_loop()
 
     def _run_core_loop(self) -> None:
@@ -30,33 +39,13 @@ class Core(object):
 
 
 def main():
-    # # COM5
-    # a1_socket = A1Socket("/dev/serial0")
-    # a1_socket.open()
-    #
-    # robot = Robot(a1_socket)
-
+    # COM5
+    a1_socket = A1Socket("/dev/serial0")
+    robot = Robot(a1_socket)
     wifi_communicator = WifiCommunicator(WIFI_SOCKET_PORT)
     wifi_endpoints_handler = WifiEndpointsHandler(wifi_communicator, wifi_module_logger)
-    core = Core(wifi_endpoints_handler)
+    core = Core(robot, wifi_endpoints_handler)
     core.run()
-    # wifi_request_handler_service = RequestHandlerService(wifi_communicator, wifi_module_logger)
-    # wifi_request_handler_service.register(HelloWorldRequest())
-    # wifi_request_handler_service.register(GetRobotSysInfo())
-    # wifi_request_handler_service.register(Motor(robot))
-    # wifi_request_handler_service.register(Movement(robot))
-    # wifi_request_handler_service.register(StopMovement(robot))
-    # wifi_request_handler_service.register(GetA1DataRequestHandler(robot))
-    #
-    # robot.core_is_initialized(True).expect()
-    #
-    # wifi_communicator.accept_connection()
-    # wifi_request_handler_service.start()
-    #
-    # while True:
-    #     if wifi_request_handler_service.is_connection_closed:
-    #         wifi_communicator.accept_connection()
-    #         wifi_request_handler_service.start()
 
 
 if __name__ == '__main__':
