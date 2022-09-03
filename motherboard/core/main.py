a69ac5f11e1ee35d56a57e46fa88ae3521ee57fe
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2022-09-03 16:04:16 +0200

    Core: Wrap some entrypoints into try-catch

--- motherboard/core/main.py
+++ motherboard/core/main.py
@@ -1,11 +1,11 @@
-
+from a1.models import ButtonState
 from a1.robot import Robot
 from a1.socket import A1Socket
 from bluetooth.handler import BluetoothEndpointsHandler
 from wifi.comunicator import WifiCommunicator
 from wifi.endpoints.a1_data import GetA1DataRequestHandler
 from wifi.endpoints.hello_world import HelloWorldRequest
-from utils.logger import wifi_module_logger
+from utils.logger import wifi_module_logger, CoreLogger
 from wifi.endpoints.motor import Motor
 from wifi.endpoints.movement import Movement, StopMovement
 from wifi.endpoints.sys_info import GetRobotSysInfo
@@ -16,10 +16,12 @@ WIFI_SOCKET_PORT = 1489
 
 class Core(object):
     def __init__(self, robot: Robot, wifi_endpoints_handler: WifiEndpointsHandler,
-                 bluetooth_endpoint_handler: BluetoothEndpointsHandler):
+                 bluetooth_endpoint_handler: BluetoothEndpointsHandler, logger: CoreLogger, **kwargs):
+        self._debug = bool(kwargs.get('debug'))
         self._robot = robot
         self._wifi_endpoints_handler = wifi_endpoints_handler
         self._bluetooth_endpoint_handler = bluetooth_endpoint_handler
+        self._logger = logger
         self._wifi_endpoints_handler.register_endpoint(HelloWorldRequest())
         self._wifi_endpoints_handler.register_endpoint(HelloWorldRequest())
         self._wifi_endpoints_handler.register_endpoint(GetRobotSysInfo())
@@ -29,23 +31,60 @@ class Core(object):
         self._wifi_endpoints_handler.register_endpoint(GetA1DataRequestHandler(self._robot))
 
     def run(self) -> None:
-        self._robot.connect()
+        self._logger.print_entry_point()
+
+        try:
+            self._robot.connect()
+        except Exception as error:
+            self._logger.critical(f'Cannot establish connection with A1 module. Reason:{error}')
+            if self._debug:
+                raise error
+            return None
+
         self._wifi_endpoints_handler.start()
-        self._run_core_loop()
+
+        try:
+            self._run_core_loop()
+        except Exception as error:
+            self._logger.critical(f'Core loop is failed. Reason: {error}')
+            if self._debug:
+                raise error
 
     def _run_core_loop(self) -> None:
         while True:
-            pass
+            but = self._robot.data.button_up
+            if but is ButtonState.CLICK:
+                print('UP CLICK')
+            elif but is ButtonState.LONG_PRESS:
+                print('UP LONG PRESS')
+
+            but2 = self._robot.data.button_down
+            if but2 is ButtonState.CLICK:
+                print('DOWN CLICK')
+            elif but2 is ButtonState.LONG_PRESS:
+                print('DOWN LONG PRESS')
+
+            but3 = self._robot.data.button_ok
+            if but3 is ButtonState.CLICK:
+                print('OK CLICK')
+            elif but3 is ButtonState.LONG_PRESS:
+                print('OK LONG PRESS')
+
+            but4 = self._robot.data.bluetooth_button
+            if but4 is ButtonState.CLICK:
+                print('B CLICK')
+            elif but4 is ButtonState.LONG_PRESS:
+                print('B LONG PRESS')
 
 
 def main():
-    # COM5
-    a1_socket = A1Socket("/dev/serial0")
+    # COM5 /dev/serial0
+    a1_socket = A1Socket("COM5")
     robot = Robot(a1_socket)
     wifi_communicator = WifiCommunicator(WIFI_SOCKET_PORT)
     wifi_endpoints_handler = WifiEndpointsHandler(wifi_communicator, wifi_module_logger)
     bluetooth_endpoints_handler = BluetoothEndpointsHandler()
-    core = Core(robot, wifi_endpoints_handler, bluetooth_endpoints_handler)
+    core = Core(robot, wifi_endpoints_handler, bluetooth_endpoints_handler, CoreLogger(True), debug=False)
     core.run()
 
 
