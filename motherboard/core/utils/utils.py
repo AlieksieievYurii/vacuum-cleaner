9ed70bda4b0b3751a36658c10f45665bf63c6a5a
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2022-09-05 23:23:16 +0200

    Core: create utils function to set data time and check if NTP is enabled

--- motherboard/core/utils/utils.py
+++ motherboard/core/utils/utils.py
@@ -1,4 +1,8 @@
+import os
+import re
+import subprocess
 import time
+from sys import platform
 
 
 def millis() -> int:
@@ -17,3 +21,33 @@ def boolean(value) -> bool:
             raise ValueError(f'Cannot convert string value "{value}" to Boolean')
     else:
         raise ValueError(f'Cannot "{value}" to Boolean')
+
+
+def os_set_data_time(data_time: str) -> None:
+    """
+    Runs OS command to set up data time using the following format Y-d-m H:i:s
+    It handles Linux and Windows. (Temporary) However, for windows it is ignore because dev is done on windows
+
+    :param data_time: string representation of data time. Example: 2000-06-02 08:47:46
+    :return: None
+    """
+
+    if platform == "linux" or platform == "linux2":
+        subprocess.run(['timedatectl', 'set-time', data_time], check=True)
+    elif platform == "win32":
+        print(f'IGNORE installing time for windows. Date time: {data_time}')
+    else:
+        raise Exception('Unknown OS to setup date time')
+
+
+def is_ntp_synchronized() -> bool:
+    """
+    Checks if NTP is synchronized
+
+    :return: True if yes, otherwise False
+    """
+
+    out: str = subprocess.run(['timedatectl', 'status'], capture_output=True, text=True, check=True).stdout
+    match = re.findall(r'\s+(System clock synchronized: yes)\s+', out)
+
+    return bool(match)
