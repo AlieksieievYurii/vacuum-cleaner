2f07951be3db3342b511cd469efa796fbeb2421c
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2022-09-27 14:57:57 +0200

    AndroidApp: Create Robot intrface

---
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/robot/Robot.kt
@@ -0,0 +1,14 @@
+package com.yurii.vaccumcleaner.robot
+
+interface Robot {
+    suspend fun walkForward(speedCmPerMinute: Int)
+    suspend fun walkBackward(speedCmPerMinute: Int)
+    suspend fun rotateLeft(speedCmPerMinute: Int)
+    suspend fun rotateRight(speedCmPerMinute: Int)
+    suspend fun stopMovement(withBreak: Boolean)
+    suspend fun setVacuumMotor(value: Int)
+    suspend fun setMainBrushMotor(value: Int)
+    suspend fun setLeftBrushMotor(value: Int)
+    suspend fun setRightBrushMotor(value: Int)
+    suspend fun getRobotInputData(): RobotInputData
+}
--- android-app/app/src/main/java/com/yurii/vaccumcleaner/robot/RobotWifiImpl.kt
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/robot/RobotWifiImpl.kt
@@ -13,33 +13,33 @@ data class MovementRequestModel(val direction: String, val speed: Int)
 @JsonClass(generateAdapter = true)
 data class StopMovementRequestModel(@Json(name = "with_break") val withBreak: Boolean)
 
-class RobotWifiImplementation(private val requestHandler: RequestHandler) {
+class RobotWifiImplementation(private val requestHandler: RequestHandler) : Robot {
 
     suspend fun getSysInfo(): GeneralSystemInfo {
         return requestHandler.send("/get-sys-info", null, GeneralSystemInfo::class.java)!!
     }
 
-    suspend fun walkForward(speedCmPerMinute: Int) = walk("forward", speedCmPerMinute)
+    override suspend fun walkForward(speedCmPerMinute: Int) = walk("forward", speedCmPerMinute)
 
-    suspend fun walkBackward(speedCmPerMinute: Int) = walk("backward", speedCmPerMinute)
+    override suspend fun walkBackward(speedCmPerMinute: Int) = walk("backward", speedCmPerMinute)
 
-    suspend fun rotateLeft(speedCmPerMinute: Int) = walk("left", speedCmPerMinute)
+    override suspend fun rotateLeft(speedCmPerMinute: Int) = walk("left", speedCmPerMinute)
 
-    suspend fun rotateRight(speedCmPerMinute: Int) = walk("right", speedCmPerMinute)
+    override suspend fun rotateRight(speedCmPerMinute: Int) = walk("right", speedCmPerMinute)
 
-    suspend fun stopMovement(withBreak: Boolean) {
+    override suspend fun stopMovement(withBreak: Boolean) {
         requestHandler.send<Any>("/stop-movement", StopMovementRequestModel(withBreak), null)
     }
 
-    suspend fun setVacuumMotor(value: Int) = setMotor("vacuum", value)
+    override suspend fun setVacuumMotor(value: Int) = setMotor("vacuum", value)
 
-    suspend fun setMainBrushMotor(value: Int) = setMotor("main_brush", value)
+    override suspend fun setMainBrushMotor(value: Int) = setMotor("main_brush", value)
 
-    suspend fun setLeftBrushMotor(value: Int) = setMotor("left_brush", value)
+    override suspend fun setLeftBrushMotor(value: Int) = setMotor("left_brush", value)
 
-    suspend fun setRightBrushMotor(value: Int) = setMotor("right_brush", value)
+    override suspend fun setRightBrushMotor(value: Int) = setMotor("right_brush", value)
 
-    suspend fun getRobotInputData() = requestHandler.send("/get-a1-data", null, RobotInputData::class.java)!!
+    override suspend fun getRobotInputData() = requestHandler.send("/get-a1-data", null, RobotInputData::class.java)!!
 
     private suspend fun setMotor(motorName: String, value: Int) {
         requestHandler.send<Any>("/set-motor", MotorRequestModule(motorName, value), null)
