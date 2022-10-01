0cb855c8c3dd946dd7e08c3b350ded9adb45eb07
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2022-10-01 13:07:46 +0200

    Core: create wifi endpoint to get all algo scripts

---
+++ motherboard/core/wifi/endpoints/algo_scripts.py
@@ -0,0 +1,54 @@
+from dataclasses import dataclass
+from typing import List, Any
+
+from algo.algo_manager import AlgorithmManager
+from utils.request_handler.models import RequestHandler, Request, AttributeHolder
+
+
+@dataclass
+class AlgorithmArgument(object):
+    name: str
+    value_type: str
+    default_value: Any
+    current_value: Any
+
+
+@dataclass
+class AlgorithmScript(object):
+    name: str
+    description: str
+    arguments: List[AlgorithmArgument]
+
+
+@dataclass
+class ResponseModel(object):
+    scripts: List[AlgorithmScript]
+
+
+class GetAlgorithmScriptsRequest(RequestHandler):
+    endpoint = '/get-algorithm-scripts'
+    request_model = None
+    response_model = ResponseModel
+
+    def __init__(self, algorithm_manager: AlgorithmManager):
+        self._algorithm_manager = algorithm_manager
+
+    def perform(self, request: Request, data: AttributeHolder) -> ResponseModel:
+        scripts = []
+        for script in self._algorithm_manager.get_scripts():
+            arguments_list = []
+            for argument in script['arguments']:
+                arguments_list.append(AlgorithmArgument(
+                    name=argument['name'],
+                    value_type=argument['value_type'],
+                    default_value=argument['default'],
+                    current_value=argument['value']
+                ))
+
+            scripts.append(AlgorithmScript(
+                name=script['name'],
+                description=script['description'],
+                arguments=arguments_list
+            ))
+
+        return ResponseModel(scripts=scripts)
