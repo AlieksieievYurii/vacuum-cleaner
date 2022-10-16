063ab8c0ce0a094fc9966910e5e0a3017ca183ce
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2022-10-16 16:38:17 +0200

    Core: Add endpoint to perform shut down and reboot

---
+++ motherboard/core/wifi/endpoints/power.py
@@ -0,0 +1,23 @@
+from utils.request_handler.models import RequestHandler, Field, Request, AttributeHolder
+
+
+class PowerRequestModel(object):
+    command = Field('command', str, is_required=True)
+
+
+class PowerRequestHandler(RequestHandler):
+    endpoint = '/power'
+    request_model = PowerRequestModel
+    response_model = None
+
+    def __init__(self, on_shut_down, on_reboot):
+        self._on_shut_down = on_shut_down
+        self._on_reboot = on_reboot
+
+    def perform(self, request: Request, data: AttributeHolder) -> None:
+        if data.command == 'shutdown':
+            self._on_shut_down()
+        elif data.command == 'reboot':
+            self._on_reboot()
+        else:
+            raise Exception(f'Wrong command: {data.command}')
