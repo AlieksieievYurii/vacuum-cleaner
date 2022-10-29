42a1d509cb185cf4ee0b4cfef5cdb592bc9ea648
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2022-10-29 18:12:10 +0200

    AndroidApp: Implement fetching the current WAP settings with the new API

--- android-app/app/src/main/java/com/yurii/vaccumcleaner/screens/wifi/WifiSettingsFragment.kt
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/screens/wifi/WifiSettingsFragment.kt
@@ -3,83 +3,24 @@ package com.yurii.vaccumcleaner.screens.wifi
 import android.os.Bundle
 import android.view.View
 import android.viewbinding.library.fragment.viewBinding
-import android.widget.Toast
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
-import com.yurii.vaccumcleaner.MainViewModel
+import com.yurii.vaccumcleaner.Injector
 import com.yurii.vaccumcleaner.R
 import com.yurii.vaccumcleaner.databinding.FragmentWifiSettingsBinding
-import com.yurii.vaccumcleaner.utils.observeOnLifecycle
+import com.yurii.vaccumcleaner.utils.ui.LoadingDialog
 
 
 class WifiSettingsFragment : Fragment(R.layout.fragment_wifi_settings) {
-    private val viewModel: WifiSettingsViewModel by viewModels {
-        WifiSettingsViewModel.Factory(
-            ViewModelProvider(requireActivity()).get(MainViewModel::class.java).bluetoothService
-        )
-    }
+    private val viewModel: WifiSettingsViewModel by viewModels { Injector.provideWifiSettingsViewModel(bluetoothProvider = true) }
     private val binding: FragmentWifiSettingsBinding by viewBinding()
+    private val loadingDialog by lazy { LoadingDialog(requireContext()) }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
-        viewModel.state.observeOnLifecycle(viewLifecycleOwner) { state ->
-            when (state) {
-                is WifiSettingsViewModel.State.Error -> binding.apply {
-                    loading.isVisible = false
-                    image.isVisible = true
-                    image.setImageResource(R.drawable.ic_error)
-                    hint.isVisible = true
-                    hint.text = getString(R.string.label_error_message, state.message)
-                    inputState(isEnabled = true)
-                }
-                WifiSettingsViewModel.State.LoadingWifiSettings -> binding.apply {
-                    loading.isVisible = true
-                    image.isVisible = false
-                    hint.text = getString(R.string.hint_loading_cuurent_configuration)
-                    inputState(isEnabled = false)
-                }
-                WifiSettingsViewModel.State.None -> binding.apply {
-                    hint.isVisible = false
-                    loading.isVisible = false
-                    image.isVisible = true
-                    image.setImageResource(R.drawable.ic_wifi)
-                    inputState(isEnabled = true)
-                }
-                WifiSettingsViewModel.State.ApplyingWifiSettings -> binding.apply {
-                    loading.isVisible = true
-                    image.isVisible = false
-                    hint.isVisible = true
-                    hint.text = getString(R.string.hint_applying_wifi_settings)
-                    inputState(isEnabled = false)
-                }
-            }
-        }
 
-        viewModel.eventFlow.observeOnLifecycle(viewLifecycleOwner) { event ->
-            when (event) {
-                WifiSettingsViewModel.Event.SettingsHaveBeenApplied -> Toast.makeText(
-                    requireContext(),
-                    "Wifi setup has been applied!",
-                    Toast.LENGTH_SHORT
-                ).show()
-                WifiSettingsViewModel.Event.CurrentSetupHasBeenReturned -> Toast.makeText(
-                    requireContext(),
-                    "Current Wifi setup has been returned",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
-    }
-
-    private fun inputState(isEnabled: Boolean) {
-        binding.apply {
-            ssid.isEnabled = isEnabled
-            password.isEnabled = isEnabled
-            apply.isEnabled = isEnabled
-        }
+        loadingDialog.observeState(viewModel.isLoading, viewLifecycleOwner)
     }
 }
--- android-app/app/src/main/java/com/yurii/vaccumcleaner/screens/wifi/WifiSettingsViewModel.kt
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/screens/wifi/WifiSettingsViewModel.kt
@@ -4,76 +4,57 @@ import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import com.yurii.vaccumcleaner.*
-import com.yurii.vaccumcleaner.service.Service
+import com.yurii.vaccumcleaner.robot.RobotWifiImplementation
 import com.yurii.vaccumcleaner.utils.Empty
-import com.yurii.vaccumcleaner.utils.value
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
-import kotlinx.coroutines.launch
-import java.lang.Exception
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 
 
-class WifiSettingsViewModel(_service: Service) : ViewModel() {
-    private val service = ServiceWrapper(_service)
-
-    sealed class State {
-        object None : State()
-        object LoadingWifiSettings : State()
-        object ApplyingWifiSettings : State()
-        data class Error(val message: String) : State()
-    }
-
+class WifiSettingsViewModel(private val robot: RobotWifiImplementation) : ViewModel() {
     sealed class Event {
-        object CurrentSetupHasBeenReturned : Event()
-        object SettingsHaveBeenApplied : Event()
+        data class ShowError(val error: Throwable) : Event()
     }
+
+    private val _isLoading: MutableStateFlow<Boolean> = MutableStateFlow(true)
+    val isLoading = _isLoading.asStateFlow()
+
+    private val _event = MutableSharedFlow<Event>()
+    val event = _event.asSharedFlow()
 
     val ssidField = ObservableField(String.Empty)
     val passwordField = ObservableField(String.Empty)
 
-    private val _state: MutableStateFlow<State> = MutableStateFlow(State.LoadingWifiSettings)
-    val state = _state.asStateFlow()
+    private val errorHandler = CoroutineExceptionHandler { _, error ->
+        _isLoading.value = false
+        sendEvent(Event.ShowError(error))
+    }
 
-    private val eventChannel = Channel<Event>(Channel.BUFFERED)
-    val eventFlow = eventChannel.receiveAsFlow()
+    private val viewModelJob = SupervisorJob()
+    private val netWorkScope = CoroutineScope(viewModelJob + Dispatchers.IO + errorHandler)
 
+    private fun sendEvent(event: Event) = viewModelScope.launch {
+        _event.emit(event)
+    }
 
     init {
-        viewModelScope.launch {
-            try {
-                val wifiSettings = service.getCurrentWifiSettings()
-                _state.value = State.None
-                ssidField.set(wifiSettings.ssid)
-                passwordField.set(wifiSettings.password)
-            } catch (error: Exception) {
-                _state.value = State.Error(error.message ?: "No error message!")
-            }
+        netWorkScope.launch {
+            _isLoading.value = true
+            val currentWpaConfig = robot.getCurrentWpaConfig()
+            ssidField.set(currentWpaConfig.ssid)
+            passwordField.set(currentWpaConfig.password)
+            _isLoading.value = false
         }
     }
 
     fun applyWifiSettings() {
-        viewModelScope.launch {
-            _state.value = State.ApplyingWifiSettings
-            try {
-                val resp = service.setWifiSettings(WifiSettings(ssid = ssidField.value, password = passwordField.value))
-                if (resp.isConnected) {
-                    _state.value = State.None
-                    eventChannel.send(Event.SettingsHaveBeenApplied)
-                }
-            } catch (error: Exception) {
-                _state.value = State.Error(error.message ?: "No error message!")
-            }
-        }
+
     }
 
-    class Factory(private val service: Service) : ViewModelProvider.Factory {
+    class Factory(private val robot: RobotWifiImplementation) : ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
             if (modelClass.isAssignableFrom(WifiSettingsViewModel::class.java)) {
                 @Suppress("UNCHECKED_CAST")
-                return WifiSettingsViewModel(service) as T
+                return WifiSettingsViewModel(robot) as T
             }
             throw IllegalArgumentException("Unable to construct viewModel")
         }
