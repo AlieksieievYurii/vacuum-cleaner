472a1a121f123b59f012c19a64d1858efc063eda
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2022-10-29 16:24:37 +0200

    Core: Add endpoint to get current WPA

--- motherboard/core/blservice/endpoints/wifi.py
+++ motherboard/core/blservice/endpoints/wifi.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from utils.os import OperationSystem
 from utils.request_handler.models import RequestHandler, Request, AttributeHolder, Field
 
@@ -5,6 +7,13 @@ from utils.request_handler.models import RequestHandler, Request, AttributeHolde
 class SetWifiCredentialsRequestModel(object):
     ssid = Field('ssid', str, is_required=True)
     password = Field('password', str, is_required=True)
+
+
+@dataclass
+class WifiCredentialsResponseModel(object):
+    ssid: str
+    password: str
+    key_mgmt: str
 
 
 class SetWifiCredentialsRequestHandler(RequestHandler):
@@ -17,3 +26,20 @@ class SetWifiCredentialsRequestHandler(RequestHandler):
 
     def perform(self, request: Request, data: AttributeHolder):
         self._os.set_wifi_credentials(data.ssid, data.password)
+
+
+class GetCurrentWifiCredentialsRequestHandler(RequestHandler):
+    endpoint = '/get-current-wifi-credentials'
+    request_model = None
+    response_model = WifiCredentialsResponseModel
+
+    def __init__(self, os: OperationSystem):
+        self._os = os
+
+    def perform(self, request: Request, data: AttributeHolder):
+        wpa_conf = self._os.get_wpa_config()
+        return WifiCredentialsResponseModel(
+            ssid=wpa_conf['ssid'],
+            password=wpa_conf['psk'],
+            key_mgmt=wpa_conf['key_mgmt']
+        )
