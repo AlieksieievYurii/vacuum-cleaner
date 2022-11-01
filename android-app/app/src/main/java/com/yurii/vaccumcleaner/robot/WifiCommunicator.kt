5e09e42a928851d18912b1a278d0301fc97fb97c
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2022-11-01 15:25:28 +0100

    AndroidApp: Fix the issue with lateinit

--- android-app/app/src/main/java/com/yurii/vaccumcleaner/robot/WifiCommunicator.kt
+++ android-app/app/src/main/java/com/yurii/vaccumcleaner/robot/WifiCommunicator.kt
@@ -6,32 +6,35 @@ import kotlinx.coroutines.withContext
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.PrintWriter
+import java.lang.IllegalStateException
 import java.net.InetSocketAddress
 import java.net.Socket
 
 class WifiCommunicator : Communicator {
-    private lateinit var socket: Socket
-    private lateinit var socketReader: BufferedReader
+    private var socket: Socket? = null
+    private var socketReader: BufferedReader? = null
 
     suspend fun connect(ip: String, port: Int) = withContext(Dispatchers.IO) {
-        socket = Socket()
-        socket.connect(InetSocketAddress(ip, port), 4000)
-        socketReader = BufferedReader(InputStreamReader(socket.inputStream))
+        socket = Socket().apply {
+            connect(InetSocketAddress(ip, port), 4000)
+        }
+        socketReader = BufferedReader(InputStreamReader(socket!!.inputStream))
     }
 
-    fun isConnected() = socket.isConnected
+    fun isConnected() = socket?.isConnected ?: false
 
     override fun read(): String {
-        return socketReader.readLine()
+        return socketReader?.readLine() ?: throw IllegalStateException("Can not read from the socket because it is null. You need to connect first")
     }
 
     override fun send(data: String) {
-        val writer = PrintWriter(socket.outputStream, true)
+        val outStream = socket?.outputStream ?: throw IllegalStateException("Cant print to the socket because it is null. You need to connect first")
+        val writer = PrintWriter(outStream, true)
         writer.println(data)
     }
 
     override fun close() {
-        socket.close()
+        socket?.close()
     }
 
 }
