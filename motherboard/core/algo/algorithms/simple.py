9f2dec226c554bfd57d113070ecec3f21c29ac13
Yurii Alieksieiev <alieksieiev.yurii@gmail.com>
2022-11-20 11:22:58 +0100

    Implement cliff events

--- motherboard/core/algo/algorithms/simple.py
+++ motherboard/core/algo/algorithms/simple.py
@@ -1,5 +1,7 @@
 import random
+from typing import Optional
 
+from a1.models import Job
 from a1.robot import Robot
 from algo.algorithms.algorithm import Algorithm, FieldParameter, ArgumentsHolder, ExecutionState
 
@@ -14,6 +16,7 @@ class Simple(Algorithm):
     reverse_dis = FieldParameter('reverse_distance', range(0, 1000), default=20)
     reverse_dis_speed = FieldParameter('reverse_distance_speed', range(0, 2000), default=1000)
     reverse_with_break = FieldParameter('reverse_with_break', bool, default=False)
+    cliff_enabled = FieldParameter('cliffs', bool, default=False)
 
     vacuum_motor_value = FieldParameter('vacuum_motor', range(0, 100), default=50)
 
@@ -29,6 +32,8 @@ class Simple(Algorithm):
                 self._correct_speed(robot)
                 if self._check_bumpers(robot, state):
                     break
+                if self._args.cliff_enabled:
+                    self._check_cliffs(robot)
 
     def _check_bumpers(self, robot: Robot, state: ExecutionState) -> bool:
         if robot.data.end_right_trig or robot.data.end_left_trig:
@@ -55,30 +60,45 @@ class Simple(Algorithm):
             self._is_lower_speed = False
 
     def on_prepare(self, robot: Robot):
-        robot.set_main_brush_motor(50).expect()
-        robot.set_left_brush_motor(70).expect()
-        robot.set_right_brush_motor(70).expect()
+        self._start_motors(robot)
+
+    def _start_motors(self, robot: Robot) -> None:
+        mbm_job: Job = robot.set_main_brush_motor(50)
+        lbm_job: Job = robot.set_left_brush_motor(70)
+        rbm_job: Job = robot.set_right_brush_motor(70)
         robot.set_vacuum_motor(int(self._args.vacuum_motor_value)).expect()
+        mbm_job.expect()
+        lbm_job.expect()
+        rbm_job.expect()
 
     def on_pause(self, robot: Robot):
-        robot.set_vacuum_motor(0).expect()
-        robot.set_main_brush_motor(30).expect()
-        robot.stop_movement(with_break=True)
-        robot.set_left_brush_motor(10).expect()
-        robot.set_right_brush_motor(10).expect()
+        robot.stop_movement(with_break=True).expect()
+
+        vm_job: Job = robot.set_vacuum_motor(0)
+        mbm_job: Job = robot.set_main_brush_motor(30)
+        lbm_job: Job = robot.set_left_brush_motor(10)
+        rbm_job: Job = robot.set_right_brush_motor(10)
+
+        vm_job.expect()
+        mbm_job.expect()
+        lbm_job.expect()
+        rbm_job.expect()
 
     def on_resume(self, robot: Robot):
-        robot.set_left_brush_motor(70).expect()
-        robot.set_right_brush_motor(70).expect()
-        robot.set_main_brush_motor(50).expect()
-        robot.set_vacuum_motor(int(self._args.vacuum_motor_value))
+        self._start_motors(robot)
 
     def on_finish(self, robot: Robot):
         robot.stop_movement(with_break=True).expect()
-        robot.set_vacuum_motor(0).expect()
-        robot.set_left_brush_motor(0).expect()
-        robot.set_right_brush_motor(0).expect()
-        robot.set_main_brush_motor(0).expect()
+
+        vm_job: Job = robot.set_vacuum_motor(0)
+        mbm_job: Job = robot.set_main_brush_motor(0)
+        lbm_job: Job = robot.set_left_brush_motor(0)
+        rbm_job: Job = robot.set_right_brush_motor(0)
+
+        vm_job.expect()
+        mbm_job.expect()
+        lbm_job.expect()
+        rbm_job.expect()
 
     def _move_backward(self, robot: Robot) -> None:
         robot.move_backward(self._args.reverse_dis, self._args.reverse_dis_speed,
@@ -89,3 +109,33 @@ class Simple(Algorithm):
 
     def _turn_right(self, robot: Robot, angle: int):
         robot.turn_right(angle, self._args.speed, True).expect()
+
+    def _check_cliffs(self, robot: Robot):
+        if robot.data.front_center_cliff_breakage:
+            self._step_aside_and_rotate(robot, None)
+        elif robot.data.front_left_cliff_breakage:
+            robot.move_backward(20, self._args.reverse_dis_speed, self._args.reverse_with_break).expect()
+            self._turn_right(robot, random.randint(30, 180))
+        elif robot.data.front_right_cliff_breakage:
+            robot.move_backward(20, self._args.reverse_dis_speed, self._args.reverse_with_break).expect()
+            self._turn_left(robot, random.randint(30, 180))
+
+    def _step_aside_and_rotate(self, robot: Robot, rotate_left: Optional[bool]):
+        def await_and_check(job):
+            while True:
+                if job.response and robot.data.back_center_cliff_breakage or robot.data.back_left_cliff_breakage \
+                        or robot.data.back_right_cliff_breakage:
+                    break
+
+        bm_job = robot.move_backward(20, self._args.reverse_dis_speed, self._args.reverse_with_break)
+        await_and_check(bm_job)
+        angle = random.randint(10, 180)
+        if rotate_left is None:
+            if random_true():
+                await_and_check(robot.turn_right(angle, self._args.speed, True))
+            else:
+                await_and_check(robot.turn_left(angle, self._args.speed, True))
+        elif rotate_left:
+            await_and_check(robot.turn_left(angle, self._args.speed, True))
+        else:
+            await_and_check(robot.turn_right(angle, self._args.speed, True))
